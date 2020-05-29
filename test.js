727aedb54d88b64a73d85c4ea8e98155ca6770f9
JaredMathis <jaredmathis@protonmail.com>
2020-05-28 21:52:11 -0400

    refactor

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 
-require("./tests/dl/dl.js");
+require("./tests/downloadFromServer/downloadFromServer.js");
 require("./tests/getServer/getServer.js");
 require("./tests/uploadToServer/uploadToServer.js");
