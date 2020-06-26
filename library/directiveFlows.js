84751b932a25527dd82d775e6fe40175e08eb520
JaredMathis <64503422+JaredMathis@users.noreply.github.com>
2020-06-26 15:20:49 -0400

    style

--- library/directiveFlows.js
+++ library/directiveFlows.js
@@ -59,64 +59,73 @@ function directiveFlows() {
             <button 
                 type="button" 
                 class="btn btn-primary"
-                ng-click="createNewFlow()">
-                Create New Flow
-            </button>
-            <button 
-                type="button" 
-                class="btn btn-primary"
                 ng-click="data()">
                 Data
             </button>
-            <button 
-                type="button" 
-                class="btn btn-primary"
-                ng-click="runAllTests()">
-                Run All Tests
-            </button>
-            Last ran: {{getState().runAllTestsLastRun || 'Never'}}
-            - {{ allSuccessfulTests().length }} 
-            out of {{ getState().tests.length }} successful
-            <table class="table">
-                <tbody>
-                    <tr 
-                    ng-repeat="flow in state.flows track by $index"
-                    ng-class="{ 
-                        'table-success': getTests(flow).length >= 1 && getSuccessfulTests(flow).length == getTests(flow).length,
-                        'table-danger': getTests(flow).length >= 1 && getSuccessfulTests(flow).length != getTests(flow).length,
-                        'table-warning': getTests(flow).length == 0,
-                    }">
-                        <td>
-                            <b>{{flow.name}}</b>
-                            (&nbsp;<span ng-repeat="i in flow.inputs"><span ng-show="!$first">&nbsp;, </span>{{ i.name }}</span>&nbsp;)&nbsp;=>
-                            (&nbsp;<span ng-repeat="o in flow.outputs"><span ng-show="!$first">&nbsp;, </span>{{ o.name }}</span>&nbsp;)
-                        </td>
-                        <td>
 
-                        <button 
-                            ng-click="editFlow(flow)"
-                            type="button" 
-                            class="btn btn-primary">
-                            Edit Flow
-                        </button>
+            <div class="card mt-1">
+                <div class="card-body">
+                    <h4 class="card-title">Flows</h4>
 
-                        <button 
-                            ng-click="testFlow(flow)"
-                            type="button" 
-                            class="btn btn-primary">
-                            Tests ({{ getTests(flow).length }})
-                        </button>
+                    <button 
+                        type="button" 
+                        class="btn btn-primary"
+                        ng-click="runAllTests()">
+                        Run All Tests
+                    </button>
+                    Last ran: {{getState().runAllTestsLastRun || 'Never'}}
+                    - {{ allSuccessfulTests().length }} 
+                    out of {{ getState().tests.length }} successful
 
-                        <button 
-                            ng-click="deleteFlow(flow)"
-                            type="button" 
-                            class="btn btn-danger">
-                            Delete Flow
-                        </button>
-                        </td>
-                    <tr>
-                </tbody>
-            </table>
+                    <br>
+                    <button 
+                        type="button" 
+                        class="btn btn-primary mt-1"
+                        ng-click="createNewFlow()">
+                        Create New Flow
+                    </button>
+                    <table class="table mt-1">
+                        <tbody>
+                            <tr 
+                            ng-repeat="flow in state.flows track by $index"
+                            ng-class="{ 
+                                'table-success': getTests(flow).length >= 1 && getSuccessfulTests(flow).length == getTests(flow).length,
+                                'table-danger': getTests(flow).length >= 1 && getSuccessfulTests(flow).length != getTests(flow).length,
+                                'table-warning': getTests(flow).length == 0,
+                            }">
+                                <td>
+                                    <b>{{flow.name}}</b>
+                                    (&nbsp;<span ng-repeat="i in flow.inputs"><span ng-show="!$first">&nbsp;, </span>{{ i.name }}</span>&nbsp;)&nbsp;=>
+                                    (&nbsp;<span ng-repeat="o in flow.outputs"><span ng-show="!$first">&nbsp;, </span>{{ o.name }}</span>&nbsp;)
+                                </td>
+                                <td>
+
+                                <button 
+                                    ng-click="editFlow(flow)"
+                                    type="button" 
+                                    class="btn btn-primary">
+                                    Edit Flow
+                                </button>
+
+                                <button 
+                                    ng-click="testFlow(flow)"
+                                    type="button" 
+                                    class="btn btn-primary">
+                                    Tests ({{ getTests(flow).length }})
+                                </button>
+
+                                <button 
+                                    ng-click="deleteFlow(flow)"
+                                    type="button" 
+                                    class="btn btn-danger">
+                                    Delete Flow
+                                </button>
+                                </td>
+                            <tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
             `
         }
     });
